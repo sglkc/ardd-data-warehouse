c9a4e0d0e7deb3ac573f783ee88a22ecfd18887e
Seya <31957516+sglkc@users.noreply.github.com>
2025-09-14 13:41:27 +0700

    feat(sqlmesh): add time_key to fact_fatalities

--- sqlmesh_project/models/gold/fact_fatalities.sql
+++ sqlmesh_project/models/gold/fact_fatalities.sql
@@ -5,6 +5,9 @@ MODEL (
 
 SELECT
   p.person_key,
-  f.*
+  TO_CHAR(c.time, 'HH24MI') AS time_key
 FROM silver.fatalities f
-LEFT JOIN gold.dim_person p ON p.person_key = @GENERATE_SURROGATE_KEY(f.road_user, f.age, f.gender);
+LEFT JOIN silver.crashes c
+  ON c.crash_id = f.crash_id
+LEFT JOIN gold.dim_person p
+  ON p.person_key = @GENERATE_SURROGATE_KEY(f.road_user, f.age, f.gender)
